d94a51c0238fc912cf1ad257e60b73540f0babab
Sandra C. <sandra.colomer@outlook.fr>
2024-10-13 15:14:03 +0200

    Slider : Fix index increment

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -11,7 +11,8 @@ const Slider = () => {
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
+    setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
+    // setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
   };
   useEffect(() => {
     nextCard();
@@ -43,6 +44,7 @@ const Slider = () => {
                   type="radio"
                   name="radio-button"
                   checked={idx === radioIdx}
+                  
                 />
               ))}
             </div>
